830318e94d686ed9fb19c64247ad51e6572a6e50
centosopenui <bobo_8318@163.com>
2019-12-09 00:04:13 +0800

    init

---
+++ imba_vuepress/docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    port: '7777',
+    title: 'IMBA.PUB',
+    description: 'imbapub',
+    head: [ // 注入到当前页面的 HTML <head> 中的标签
+      ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ],
+    base: '/', // 这是部署到github相关的配置
+    markdown: {
+      lineNumbers: true // 代码块显示行号
+    },
+    //theme: 'vuepress-theme-yilia-plus',
+    themeConfig: {
+      nav:[ // 导航栏配置
+        {text: '前端基础', link: '/accumulate/' },
+        {text: '算法题库', link: '/algorithm/'},
+        {text: '微博', link: 'https://baidu.com'}      
+      ],
+      sidebar: 'auto', // 侧边栏配置
+      sidebarDepth: 2, // 侧边栏显示2级
+    },
+    head: [ // 注入到当前页面的 HTML <head> 中的标签
+        ['link', { rel: 'manifest', href: '/photo.jpg' }],
+        ['link', { rel: 'apple-touch-icon', href: '/photo.jpg' }],
+      ],
+    serviceWorker: true // 是否开启 PWA
+  };
---
+++ imba_vuepress/docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /head.jpg
+actionText: 快速了解 →
+actionLink: /opensource/
+features:
+- title: 个人介绍
+  details: 主要涉及技术：Java后端开发、聚合支付、公众号开发、开源爱好者、Linux
+- title: 开发笔记
+  details: Java笔记、前端开发笔记、Hexo博客笔记、日记本、Linux笔记、ELK日志分析
+- title: 技术博客
+  details: 将深入探讨相关技术，包括行业动态，架构设计，设计模式，框架使用等。
+footer: &copy; 2018-2019 <a href="https://zhousiwei.gitee.io" target="_blank">試毅-思伟</a> | <a href="https://github.com/JoeyBling/vuepress-theme-yilia-plus" target="_blank">vuepress-theme-yilia-plus</a> by Litten
+---
